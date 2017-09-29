d6260d70daa171432c09f49924be1ed525824d5a
ugogo <hello@onali-ugo.fr>
2017-09-29 11:29:22 +0100

    Milligram as CSS framework

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import '../node_modules/milligram/dist/milligram.min.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
